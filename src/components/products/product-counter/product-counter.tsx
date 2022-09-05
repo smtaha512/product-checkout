153b84afdf14e8fb39f7a53efae5624f1acec545
smtaha512 <s.m.taha10@gmail.com>
2022-09-05 23:05:39 +0500

    feat: add children to be passed as prop

--- src/components/products/product-counter/product-counter.tsx
+++ src/components/products/product-counter/product-counter.tsx
@@ -1,21 +1,26 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { ProductCountDisplay } from "../product-count-display/product-count-display";
 import { ProductDecrementerButton } from "../scenarios/product-decrementer-button/product-decrementer-button";
 import { ProductIncrementorButton } from "../scenarios/product-incrementor-button/product-incrementor-button";
 import "./styles.scss";
 
-export interface ProductCounterProps { }
+export interface ProductCounterProps {
+  children: React.ReactElement;
+}
 
 export function ProductCounter(props: ProductCounterProps) {
   const [count, setCount] = useState(0);
   return (
-    <div className="d-flex flex-column align-items-center">
-      <div className="d-flex justify-content-between w-100">
-        <ProductDecrementerButton disabled={count < 1} onClick={() => setCount(count - 1)} />
-        <ProductCountDisplay count={count} />
-        <ProductIncrementorButton disabled={count > 10} onClick={() => setCount(count + 1)} />
+    <>
+    {props.children}
+      <div className="d-flex flex-column align-items-center">
+        <div className="d-flex justify-content-between w-100">
+          <ProductDecrementerButton disabled={count < 1} onClick={() => setCount(count - 1)} />
+          <ProductCountDisplay count={count} />
+          <ProductIncrementorButton disabled={count > 10} onClick={() => setCount(count + 1)} />
+        </div>
+        <p className="product-counter-text m-0">quantity</p>
       </div>
-      <p className="product-counter-text m-0">quantity</p>
-    </div>
+    </>
   );
 }
